8766e4768fd5a156290ae5eca4e3bce4162e65dc
rajeshbasnet <rajeshkhapataribasnet123@gmail.com>
2023-11-01 14:57:32 +0545

    Created pages, components,and added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
-const App = () => {
-  return <div>App</div>;
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./globals.css";
+import { Home } from "./pages";
+import SigninForm from "./_auth/SigninForm";
+import SignupForm from "./_auth/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./pages/RootLayout";
+
+const App: React.FC = () => {
+  const router = createBrowserRouter([
+    /** Public Routes */
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+      ],
+    },
+    {
+      path: "/auth",
+      element: <AuthLayout />,
+      children: [
+        {
+          path: "sign-in",
+          element: <SigninForm />,
+        },
+        {
+          path: "sign-up",
+          element: <SignupForm />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <main className="flex h-screen">
+      <RouterProvider router={router} />
+    </main>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <main className="flex h-screen">
+    <App />
+  </main>
+);
